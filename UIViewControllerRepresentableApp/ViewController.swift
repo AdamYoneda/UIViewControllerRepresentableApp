9a9e40d2072f8293257dcb651b5c24269744ffe7
Adam Yoneda <adamyone.ttlife@gmail.com>
2023-09-25 19:38:49 +0900

    Initial Commit

---
+++ UIViewControllerRepresentableApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIViewControllerRepresentableApp
+//
+//  Created by Adam Yoneda on 2023/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UIViewControllerRepresentableAppUITests/UIViewControllerRepresentableAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UIViewControllerRepresentableAppUITestsLaunchTests.swift
+//  UIViewControllerRepresentableAppUITests
+//
+//  Created by Adam Yoneda on 2023/09/25.
+//
+
+import XCTest
+
+final class UIViewControllerRepresentableAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
